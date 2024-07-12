68ecf09bb77b4a620d8515fad5f1455822d21664
Lellouche <an.lellouche@gmail.com>
2024-07-12 12:09:57 +0200

    Ajout sass ajout react router ajout structure fichiers ajout différents pages que le site contiendra ajout scss global

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
+import React from "react";
+import "./styles/main.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./base/Footer";
+import Header from "./base/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <Counter />
+    //   </header>
+    // </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/base/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/base/_header.scss"
+
+const Header = () => {
+    return (
+        <div className='header'>
+            HEADER
+        </div>
+    );
+};
+
+export default Header;
