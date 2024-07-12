a76ef9b31a92c91f3d082572fa386bec690cf75d
Lellouche <an.lellouche@gmail.com>
2024-07-12 15:35:41 +0200

    footer & header crée

--- src/base/Footer.jsx
+++ src/base/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import "../styles/base/_footer.scss";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            FOOTER
-        </div>
-    );
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
 };
 
 export default Footer;
